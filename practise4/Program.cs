ab6d99aab78bea761f7b8aa65a40e85360876eed
Fatima Hasanzada <tu7dkjdue@aztu.local>
2022-05-14 13:30:11 +0400

    BulletCapacity

---
+++ practise4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace practise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletCapacity;
+            int bulletCount;
+            bool check = true;
+
+            do
+            {
+                Console.WriteLine("BulletCapacity daxil edin");
+                bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            } while (bulletCapacity<0 || bulletCapacity == 0);
+            do
+            {
+                Console.WriteLine("BulletCount daxil edin");
+                bulletCount = Convert.ToInt32(Console.ReadLine());
+            } while (bulletCount < 0 || bulletCount>bulletCapacity || bulletCount == 0);
+            Weapon wp = new Weapon(bulletCapacity, bulletCount);
+            wp.BulletCapacity = bulletCapacity;
+            wp.BulletCount = bulletCount;
+            do
+            {
+                Console.WriteLine("0: İnformasiya almaq üçün");
+                Console.WriteLine("1: Shoot metodu üçün");
+                Console.WriteLine("2: Fire metodu üçün");
+                Console.WriteLine("3: GetRemainBulletCount metodu üçün");
+                Console.WriteLine("4: Reload metodu üçün");
+                Console.WriteLine("5: ChangeFireMode metodu üçün");
+                Console.WriteLine("6: Proqramdan dayandırmaq üçün");
+
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        break;
+                    case "1":
+                        wp.Shoot();
+                        break;
+                    case "2":
+                        wp.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine(wp.GetRemaindBulletCount()); 
+                        break;
+                    case "4":
+                        wp.Reload();
+                        break;
+                    case "5":
+                        wp.ChangeFireMode();
+                        break;
+                    case "6":
+                        check = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (check);
+
+
+
+
+           
+            
+        }
+    }
+}
---
+++ practise4/Weapon.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise4
+{
+    class Weapon
+    {
+        private int _bulletCapacity;
+        public int BulletCapacity { get => _bulletCapacity;
+            set
+            {
+                if (value>0)
+                {
+                    _bulletCapacity = value;
+                }
+            }
+        }
+        private int _bulletCount;
+        public int BulletCount
+        {
+            get => _bulletCount;
+            set
+            {
+                if (value > 0)
+                {
+                    _bulletCount = value;
+                }
+            }
+        }
+        public bool IsAutomatic { get; set; }
+        public Weapon()
+        {
+
+        }
+        public Weapon(int bulletcapacity,int bulletcount)
+        {
+            BulletCapacity = bulletcapacity;
+            BulletCount = bulletcount;
+        }
+
+        public void Shoot()
+        {
+            if (BulletCount>0)
+            {
+                Console.WriteLine(--BulletCount) ;
+            }
+            else{
+                Console.WriteLine("Gulle yoxdur");
+            }
+        }
+        public void Fire()
+        {
+            if (BulletCount > 0 )
+            {
+                if (IsAutomatic)
+                {
+                    Console.WriteLine(BulletCapacity = 0);
+                }
+                else
+                {
+                    Console.WriteLine(--BulletCount);
+                }
+            }
+        }
+        public int GetRemaindBulletCount()
+        {
+            if (BulletCapacity != BulletCount)
+            {
+                return BulletCapacity - BulletCount;
+            }
+            return BulletCount;
+        }
+
+        public void Reload()
+        {
+            int count;
+            if (BulletCount != BulletCapacity)
+            {
+                count = BulletCapacity - BulletCount;
+                BulletCount += count;
+                Console.WriteLine(BulletCount);
+            }
+            else
+            {
+                Console.WriteLine("You don't need reload");
+            }
+        }
+        public void ChangeFireMode()
+        {
+            if (IsAutomatic == true)
+            {
+                IsAutomatic = false;
+                Console.WriteLine("Firemode: " + IsAutomatic);
+            }
+            else
+            {
+                IsAutomatic = true;
+                Console.WriteLine("Firemode: " + IsAutomatic);
+
+            }
+        }
+
+    }
+}
